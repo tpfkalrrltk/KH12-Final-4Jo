7ac1fd090a79dfba28d57f02a071a2e2865e88b7
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-03 13:50:47 +0900

    초기 시작준비

---
+++ EveryFit/src/main/java/com/kh/EveryFit/EveryFitApplication.java
@@ -0,0 +1,13 @@
+package com.kh.EveryFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryFitApplication.class, args);
+	}
+
+}
---
+++ EveryFit/src/test/java/com/kh/EveryFit/EveryFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.EveryFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
