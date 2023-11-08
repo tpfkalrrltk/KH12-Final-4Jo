1996a1c8417d7b8f2df24278b153221f4a7a9299
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-08 14:35:04 +0900

    모임게시판 CRUD 구현

---
+++ EveryFit/src/main/java/com/kh/EveryFit/dao/MoimBoardDaoImpl.java
@@ -0,0 +1,45 @@
+package com.kh.EveryFit.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.EveryFit.dto.MoimBoardDto;
+
+@Repository
+public class MoimBoardDaoImpl implements MoimBoardDao {
+
+	@Autowired
+	SqlSession sqlSession;
+
+	@Override
+	public List<MoimBoardDto> list() {
+
+		return sqlSession.selectList("MoimBoard.list");
+	}
+
+	@Override
+	public void add(MoimBoardDto moimBoardDto) {
+		sqlSession.insert("MoimBoard.add", moimBoardDto);
+
+	}
+
+	@Override
+	public void edit(MoimBoardDto moimBoardDto, int moimBoardNo) {
+		Map<String, Object> params = new HashMap<>();
+		params.put("no", moimBoardNo);
+		params.put("moimBoardDto", moimBoardDto);
+		sqlSession.update("MoimBoard.edit", params);
+
+	}
+
+	@Override
+	public void delete(int moimBoardNo) {
+		sqlSession.delete("MoimBoard.delete", moimBoardNo);
+
+	}
+}
