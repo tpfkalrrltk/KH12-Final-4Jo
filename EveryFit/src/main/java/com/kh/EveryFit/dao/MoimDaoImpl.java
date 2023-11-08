4450ec12c39d95a99f522342a222874e983840ac
user7198548 <eastsluv@naver.com>
2023-11-08 17:47:45 +0900

    moimDao / moimDaoImpl 생성

---
+++ EveryFit/src/main/java/com/kh/EveryFit/dao/MoimDaoImpl.java
@@ -0,0 +1,40 @@
+package com.kh.EveryFit.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.EveryFit.dto.MoimDto;
+import com.kh.EveryFit.dto.MoimMemberDto;
+
+@Repository
+public class MoimDaoImpl implements MoimDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public MoimDto selectOne(int moimNo) {
+		return sqlSession.selectOne("moim.findAll", moimNo);
+	}
+	
+	@Override
+	public int sequence() {
+		return sqlSession.selectOne("moim.sequence");
+	}
+
+	@Override
+	public void insert(MoimDto moimDto) {
+		sqlSession.insert("moim.create", moimDto);
+	}
+	
+	@Override
+	public List<MoimMemberDto> selectAllMoimMembers(int moimNo) {
+		return sqlSession.selectList("moim.moimMemberList", moimNo);
+	}
+	
+	
+	
+}
