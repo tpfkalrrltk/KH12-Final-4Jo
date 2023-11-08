8694903c655849fa0630b7523abd4447899a6b47
yejishin95 <yeji2336@naver.com>
2023-11-08 13:30:12 +0900

    productDto 생성

---
+++ EveryFit/src/main/java/com/kh/EveryFit/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package com.kh.EveryFit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+	private int productNo;
+	private String productName;
+	private int productPrice;
+}
