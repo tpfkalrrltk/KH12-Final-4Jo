634a65a969a5f78b20df8d7b49e90e0d8641b5d6
Jelo777 <junheef1@gmail.com>
2023-11-20 22:10:49 +0900

    리그상태 업데이트 스케줄러로

--- EveryFit/src/main/java/com/kh/EveryFit/service/LeagueScheduler.java
+++ EveryFit/src/main/java/com/kh/EveryFit/service/LeagueScheduler.java
@@ -1,11 +1,11 @@
 package com.kh.EveryFit.service;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.kh.EveryFit.dao.LeagueDao;
@@ -20,21 +20,22 @@ public class LeagueScheduler {
 
 	@Autowired private LeagueDao leagueDao;
 	
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	
 	//@Scheduled(cron = "0/30 * * * * ?")
 	private void leagueStatusUpdate() {
 		List<LeagueDto> leagueList = leagueDao.selectLeagueDtoList();
 		LocalDateTime currentDate = LocalDateTime.now();
+		Date currentDateSql = Date.valueOf(currentDate.toLocalDate());
 		
 		for(LeagueDto leagueDto:leagueList) {
 			int leagueNo = leagueDto.getLeagueNo();
 			LeagueApplicationDto applicationDto = leagueDao.selectOneLeagueApplication(leagueNo);
 			if(applicationDto==null) continue;
 			LocalDateTime applicationStart = 
-					LocalDateTime.parse(applicationDto.getLeagueApplicationStart(), 
-							DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+					LocalDateTime.parse(applicationDto.getLeagueApplicationStart(), formatter);
 			LocalDateTime applicationEnd = 
-					LocalDateTime.parse(applicationDto.getLeagueApplicationEnd(), 
-							DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+					LocalDateTime.parse(applicationDto.getLeagueApplicationEnd(), formatter);
 			
 			String status;
 			if(currentDate.isBefore(applicationEnd) && currentDate.isAfter(applicationStart)) {
@@ -46,13 +47,18 @@ public class LeagueScheduler {
 			else {
 				status="대기상태";
 			}
-			log.debug("status = {}", status);
+			if(currentDateSql.before(leagueDto.getLeagueEnd()) 
+					&& currentDateSql.after(leagueDto.getLeagueStart())) {
+				status = "진행중";
+			}
+			else if(currentDateSql.after(leagueDto.getLeagueEnd())) {
+				status = "종료됨";
+			}
+			
 			if(!leagueDto.getLeagueStatus().equals(status)) {
 				leagueDto.setLeagueStatus(status);
-				log.debug("leagueDto = {}", leagueDto);
 				leagueDao.updateLeague(leagueNo, leagueDto);
 			}
 		}
 	}
-
 }
