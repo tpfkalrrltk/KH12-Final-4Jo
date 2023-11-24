c6a5f14c1250c8c4f765174d28c8e1597615feee
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-24 10:08:11 +0900

    인터셉터 오류 수정

--- EveryFit/src/main/java/com/kh/EveryFit/interceptor/MemberBlockInterceptor.java
+++ EveryFit/src/main/java/com/kh/EveryFit/interceptor/MemberBlockInterceptor.java
@@ -1,41 +1,42 @@
-//package com.kh.EveryFit.interceptor;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import com.kh.EveryFit.dao.MemberDao;
-//import com.kh.EveryFit.dto.MemberDto;
-//import com.kh.EveryFit.error.AuthorityException;
-//
-//@Component
-//public class MemberBlockInterceptor implements HandlerInterceptor {
-//
-//	@Autowired
-//	MemberDao memberDao;
-//
-//	@Override
-//	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-//			throws Exception {
-//		HttpSession session = request.getSession();
-//
-//		String memberId = (String) session.getAttribute("name");
-//		MemberDto memberDto = memberDao.selectOne(memberId);
-//		String memberBlock = memberDto.getMemberBlock();
-//
-//		boolean isBlock = memberBlock== "Y";
-//
-//		if (isBlock) {
-//			return true;
-//		} else {
-//			throw new AuthorityException("차단당한 회원입니다. 관리자에게 문의 하세요");
-//			 //esponse.sendRedirect(request.getContextPath()+"/error/MemberBlockException");
-//			// response.sendRedirect(request.getContextPath()+"/member/login");
-//			// return false;
-//		}
-//	}
-//}
+package com.kh.EveryFit.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.kh.EveryFit.dao.MemberDao;
+import com.kh.EveryFit.dto.MemberDto;
+import com.kh.EveryFit.error.AuthorityException;
+
+@Component
+public class MemberBlockInterceptor implements HandlerInterceptor {
+
+	@Autowired
+	MemberDao memberDao;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+
+		String memberId = (String) session.getAttribute("name");
+		MemberDto memberDto = memberDao.selectOne(memberId);
+		String memberBlock = memberDto.getMemberBlock();
+
+		boolean isNotBlock = memberBlock !=null && memberBlock.equals("N");
+
+		if (isNotBlock) {
+			return true;
+		} else {
+			//throw new AuthorityException("차단당한 회원입니다. 관리자에게 문의 하세요");
+			response.sendRedirect(request.getContextPath()+"/error/MemberBlockException");
+			// response.sendRedirect(request.getContextPath()+"/member/login");
+			// return false;
+		}
+		return true;
+	}
+}
